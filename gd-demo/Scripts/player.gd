31c40c587e485e7707aaa08de94bd7a78bd0e515
孙达明 <shun_@outlook.com>
2025-01-25 10:04:14 +0800

    feat:玩家控制台输出

---
+++ gd-demo/Scripts/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+# _ready函数中的代码会在游戏开始的时候运行
+func _ready() -> void:
+	print("Hello,World!")
+	# pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
