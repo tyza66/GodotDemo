bbe1cb84921542dfcfb60f24ebdb01f687e150d8
孙达明 <shun_@outlook.com>
2025-01-25 12:42:38 +0800

    feat:实现消灭史莱姆

--- gd-demo/Scripts/enemy.gd
+++ gd-demo/Scripts/enemy.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var slimer_speed : float = -100
+var is_dead : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,11 +10,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position += Vector2(slimer_speed,0) *delta # delta是每一帧有多少秒
-	pass
+	if not is_dead:
+		position += Vector2(slimer_speed,0) *delta # delta是每一帧有多少秒
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		print("敌人触碰到玩家")
 		body.game_over()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		print("史莱姆中弹了")
+		$AnimatedSprite2D.play("death") # 这样直接将属性拖进来的是按照名称寻找的,很脆弱
+		is_dead = true 
+		
+		await get_tree().create_timer(0.6).timeout
+		queue_free() # 销毁史莱姆节点
