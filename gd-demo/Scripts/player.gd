80f880db5bb12dc7f2c6e170aa46b4d5ff75d176
孙达明 <shun_@outlook.com>
2025-01-25 11:22:49 +0800

    feat:实现空气墙

--- gd-demo/Scripts/player.gd
+++ gd-demo/Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 # 变量声明
 @export var move_speed : float = 50
+@export var animator : AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 # _ready函数中的代码会在游戏开始的时候运行
@@ -13,6 +14,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # _process函数会在游戏的每一帧都执行一次
-func _process(delta: float) -> void:
+# _physics_process固定帧率可以确保不同电脑上的运行效果是一样的
+func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("left","right","up","down") * move_speed
+	
+	if velocity == Vector2.ZERO:
+		animator.play("idle")
+	else:
+		animator.play("run")
+	
 	move_and_slide()
