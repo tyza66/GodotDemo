283c09a01b53f84b29998db65e0dd0445dbd1db5
孙达明 <shun_@outlook.com>
2025-01-25 13:06:09 +0800

    feat:史莱姆生成速度加速\限位

--- gd-demo/Scripts/GameManager.gd
+++ gd-demo/Scripts/GameManager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var slime_scene : PackedScene
+@export var spawn_timer : Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,8 +9,9 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta: float) -> void: # 我们要执行的代码不涉及物理计算就直接写在process里面就行
+	spawn_timer.wait_time -= 0.2 * delta # 每秒减少0.2
+	spawn_timer.wait_time = clamp(spawn_timer.wait_time,1,3) # 将等待时间限制在1和3之间
 
 
 func _spawn_climer() -> void:
