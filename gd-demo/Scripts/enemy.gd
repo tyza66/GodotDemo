d3bebdf2b5dda3f4b030f00f079ed766f09e34a8
孙达明 <shun_@outlook.com>
2025-01-25 11:56:30 +0800

    feat:创建史莱姆敌人

---
+++ gd-demo/Scripts/enemy.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var slimer_speed : float = -100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	position += Vector2(slimer_speed,0) *delta # delta是每一帧有多少秒
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
